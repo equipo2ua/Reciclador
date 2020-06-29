68285e0cbbebd590cf9a359c754d63b737cdbfd3
AlvaroCodigo <alvaro.n.yanez.v@gmail.com>
2020-06-29 14:59:33 -0400

    Layaut : Crear solicitud, ver perfil

---
+++ app/src/main/java/com/example/reciclador/VisualizarPerfil.java
@@ -0,0 +1,14 @@
+package com.example.reciclador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisualizarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_perfil);
+    }
+}
