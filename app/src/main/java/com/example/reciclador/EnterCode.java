f5db4d60373a518df9e095f1b102ba7766d1dd91
matiasua <matiasncp7@gmail.com>
2020-06-29 01:38:22 -0400

    Create Layout
    Recuperar Cuenta, Ingresar Codigo, Nueva Contraseña

---
+++ app/src/main/java/com/example/reciclador/EnterCode.java
@@ -0,0 +1,14 @@
+package com.example.reciclador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_code);
+    }
+}
