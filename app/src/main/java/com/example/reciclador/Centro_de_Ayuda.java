33beba54a609f2fe518e3cde11834752d80f99c9
AlvaroCodigo <alvaro.n.yanez.v@gmail.com>
2020-07-06 14:01:15 -0400

    add activiti
    Se agrego al proyecto dos nuevos activitis, el Centro de Ayuda y el perfil del recolector, ademas se agrego el campo del rut en el activiti de registrar Reciclador.

---
+++ app/src/main/java/com/example/reciclador/Centro_de_Ayuda.java
@@ -0,0 +1,14 @@
+package com.example.reciclador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Centro_de_Ayuda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centro_de__ayuda);
+    }
+}
---
+++ app/src/main/java/com/example/reciclador/Visualizar_Perfil_Recolector.java
@@ -0,0 +1,14 @@
+package com.example.reciclador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Visualizar_Perfil_Recolector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar__perfil__recolector);
+    }
+}
